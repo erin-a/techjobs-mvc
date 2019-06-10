a68a655cbae996367ddf462212527babd9be51a6
erin-a <erineadler@gmail.com>
2019-06-09 21:06:36 -0500

    Completed Add Search Results Handler - can search by all or by single column

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -15,12 +15,34 @@ import java.util.HashMap;
 @Controller
 @RequestMapping("search")
 public class SearchController {
-
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
+
+    @RequestMapping(value = "results")  // would it be: value = "results/{searchType}/{searchName}"  ??
+    public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        ArrayList<HashMap<String, String>> searchresults;
+        //if searching for all do search A
+        if (searchType.equals("all")) {
+            searchresults = JobData.findByValue(searchTerm);
+        }
+        // else do search B
+        else {
+            searchresults = JobData.findByColumnAndValue(searchType, searchTerm);
+
+        }
+
+
+
+        model.addAttribute("columns", ListController.columnChoices);
+        //model.addAttribute("", ListController.???)
+        model.addAttribute("searchresults", searchresults);
+
+        return "search";
+    }
+
 
     // TODO #1 - Create handler to process search request and display results
     // The method should take in two parameters,
@@ -29,9 +51,9 @@ public class SearchController {
     // need to name them appropriately, based on the corresponding form field names.
 
     // need to use the correct annotations for the method and parameters. To configure
-    // the correct mapping route, refer to the form action in search.html.
+    // the correct mapping route, refer to the form action in search.html. (should be results)
 
-    //you'll need to pass them into the search.html view via the model.
+    //you'll need to pass them into the search.html view via the model. (addAttribute)
 
     //need to pass ListController.columnChoices to the view, as the existing search handler does.
 
